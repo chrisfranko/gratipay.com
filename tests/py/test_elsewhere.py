b810b14b2e5de2f5e095b1ad6350f3d5b29d00d1
Changaco <changaco@changaco.oy.lc>
2015-02-09 14:10:14 +0100

    factorize test and move it to `test_elsewhere.py`

--- tests/py/test_elsewhere.py
+++ tests/py/test_elsewhere.py
@@ -109,6 +109,13 @@ class TestElsewhere(Harness):
             assert response.code == 200
             assert 'has not joined' in response.body.decode('utf8')
 
+    def test_user_pages_not_found(self):
+        user_name = 'adhsjakdjsdkjsajdhksda'
+        error = "The user %s does not exist on %s"
+        for platform in self.platforms:
+            r = self.client.GxT("/on/%s/%s/" % (platform.name, user_name))
+            expected = error % (user_name, platform.display_name)
+            assert expected in r.body
 
     def test_failure_page_requires_valid_username(self):
         response = self.client.GxT('/on/twitter/nmjhgfcftyuikjnbvftyujhbgtfgh/failure.html?action')
